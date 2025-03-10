8f20ab9ec6c3e106c7a6813aa8ac134585f89656
phertyameen <aminubabafatima8@gmail.com>
2025-03-10 09:09:01 +0100

    database connection and configuration

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -4,9 +4,39 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { HealthModule } from './health/health.module';
 import { SignalsModule } from './signals/signals.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import appConfig from './config/app.config';
+import {TypeOrmModule} from '@nestjs/typeorm'
+import databaseConfig from './config/database.config';
+
+const ENV = process.env.NODE_ENV;
 
 @Module({
-  imports: [AuthModule, HealthModule, SignalsModule],
+  imports: [
+    AuthModule,
+    HealthModule,
+    SignalsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: !ENV ? '.env' : `.env.${ENV.trim()}`,
+      load: [appConfig, databaseConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('database.host'),
+        port: +configService.get('database.port'),
+        username: configService.get('database.user'),
+        password: configService.get('database.password'),
+        database: configService.get('database.name'),
+        blog: configService.get('database.blog'),
+        synchronize: configService.get('database.synchronize'),
+        autoLoadEntities: configService.get('database.autoload'),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
