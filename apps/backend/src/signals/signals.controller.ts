ac03282751cbeec0d235a4515559b103ae45f855
OthmanImam <imkargi@gmail.com>
2025-03-15 05:44:11 -0700

    Implemented the getMockSignal endpoint inside SignalsController

--- apps/backend/src/signals/signals.controller.ts
+++ apps/backend/src/signals/signals.controller.ts
@@ -1,9 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { SignalsService } from './signals.service';
+import { MockSignalService } from './mock-signals.service';
+import { TradingSignal } from 'src/interfaces/trading-signal.interface';
+
 @Controller('signals')
 export class SignalsController {
   constructor(
     private readonly signalsService: SignalsService,
+    private readonly mockSignalsService: MockSignalService
   ) {}
 
   @Get()
@@ -11,6 +15,10 @@ export class SignalsController {
     return this.signalsService.findAll();
   }
 
+  @Get('mock')
+  getMockSignals(): TradingSignal[] {
+    return this.mockSignalsService.generateMockSignals();
+  }
 }
 
 
