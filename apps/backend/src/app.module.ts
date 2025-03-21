3a3f28ab5456960faf5a1754f436644d01376ecb
portableDD <emmanueltemitopedorcas20@gmail.com>
2025-03-21 15:44:55 +0100

    "User prefrenecs"

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -12,6 +12,7 @@ import databaseConfig from './config/database.config';
 import { RedisModule } from './redis/redis.module';
 import { RedisController } from './redis/redis.controller';
 import { SignalGateway } from './gateways/signal.gateway';
+import { UserModule } from './user/user.module';
 
 const ENV = process.env.NODE_ENV;
 console.log(ENV);
@@ -45,6 +46,7 @@ console.log(ENV);
     }),
     StarknetModule,
     RedisModule,
+    UserModule,
   ],
   controllers: [AppController, RedisController],
   providers: [AppService, SignalGateway],
