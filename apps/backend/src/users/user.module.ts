14a9af3f018b30fa0b06d464c502bbc3beba439f
peteroche <petergoddey08@gmail.com>
2025-03-25 14:52:40 +0100

    fixed monorepo issue

--- apps/backend/src/users/user.module.ts
+++ apps/backend/src/users/user.module.ts
@@ -4,9 +4,13 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { User } from './entities/user.entity';
 import { RedisService } from '../redis/redis.service';
+import { GatewayModule } from '../gateways/gateway.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    GatewayModule
+  ],
   controllers: [UserController],
   providers: [UserService, RedisService],
   exports: [UserService]
